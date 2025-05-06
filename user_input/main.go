108736eeda112acd1ef7f9400e0d747e10c56d7f
techyans <techyans96@gmail.com>
2025-05-07 01:15:54 +0530

    Learn interface

--- user_input/main.go
+++ user_input/main.go
@@ -4,12 +4,34 @@ import (
 	"bufio"
 	"fmt"
 	"go_course/note"
+	"go_course/todo"
 	"os"
 	"strings"
 )
 
+type saver interface {
+	Save() error
+}
+
 func main() {
 	title, content := GetNoteData()
+
+	todotext := GetUserInput("Todo: ")
+
+	todo, err := todo.New(todotext)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	todo.Display()
+	err = todo.Save()
+	if err != nil {
+		fmt.Println("Error in todo saving", err)
+		return
+	}
+
+	fmt.Println("tode saved successfully")
 
 	UserNote, err := note.New(title, content)
 	if err != nil {
